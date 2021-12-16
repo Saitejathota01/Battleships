cce2c7d2ade0d397d47a5881abf87c499dd96ae5
sai teja <saitejathota134@msitprogram.net>
2021-12-16 13:30:11 +0530

    CheckShip

--- battleship.py
+++ battleship.py
@@ -62,13 +62,7 @@ Parameters: int ; int
 Returns: 2D list of ints
 '''
 def emptyGrid(rows, cols):
-    a=[] 
-    for i in range(rows):
-        b=[] 
-        for j in range(cols):
-            b.append(EMPTY_UNCLICKED)
-        a.append(b) 
-    return a
+    return
     
 
 
@@ -79,7 +73,18 @@ Returns: 2D list of ints
 '''
 
 def createShip():
-    return
+    r=random.randint(1,8)
+    c=random.randint(1,8)
+    b=random.randint(0,1)
+    if b == 0:
+        ship=[[r-1,c],[r,c],[r+1,c]]
+    else:
+        ship=[[r,c-1],[r,c],[r,c+1]]
+    return ship
+print(createShip());
+
+
+    
 
     
 
@@ -89,8 +94,13 @@ checkShip(grid, ship)
 Parameters: 2D list of ints ; 2D list of ints
 Returns: bool
 '''
-def checkShip(grid, ship):
-    return
+def CheckShip(grid):
+    for i in range(len(ship)):
+        if grid[ship[i][0]][ship[i][1]]!=EMPTY_UNCLICKED:
+            return False
+    return True    
+
+    
 
 
 '''
@@ -99,7 +109,7 @@ Parameters: 2D list of ints ; int
 Returns: 2D list of ints
 '''
 def addShips(grid, numShips):
-    return
+   return 
 
 
 '''
@@ -281,5 +291,7 @@ if __name__ == "__main__":
     
     ## Finally, run the simulation to test it manually ##
     # runSimulation(500, 500)
-    test.testEmptyGrid()
+    test.testCheckShip()
+
+
     
